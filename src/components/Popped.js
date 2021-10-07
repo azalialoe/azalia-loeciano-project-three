421ec4fd148a0c99bbe99058f15437de051748e1
azalialoe <azalia.loeciano@gmail.com>
2021-10-07 00:27:44 -0400

    some more tiny styling change

--- src/components/Popped.js
+++ src/components/Popped.js
@@ -2,7 +2,7 @@ function Popped () {
 
     return (
         <div className="popped">
-            <h2>✨✨✨YOU GOT THIS✨✨✨</h2>
+            <h2>✨✨YOU'RE DOING A GREAT JOB!✨✨</h2>
             <h3>Stressed spelled backwards is desserts! 🍰🧁🍮🍨🍩</h3>
             <p>If you needed more balls to click, please click reset and fill the form again.</p>
             <p>Otherwise, thanks for visiting and have a joke before you leave!</p>
